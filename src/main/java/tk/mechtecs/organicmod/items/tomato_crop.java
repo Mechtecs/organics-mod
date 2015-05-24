fffe073725ad0fa49b959be3eb0c30dc64d4e86e
Mechtecs <mechtecs@gmail.com>
2015-05-25 00:01:36 +0200

    Big TOMATO UPDATE! Version 0.0.2
    Added Tomatos
    Set Version to 0.0.2

--- src/main/java/tk/mechtecs/organicmod/items/tomato_crop.java
+++ src/main/java/tk/mechtecs/organicmod/items/tomato_crop.java
@@ -2,12 +2,15 @@ package tk.mechtecs.organicmod.items;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 
-public class tomato_crop extends Item{
+public class tomato_crop extends ItemFood{
 	
 	public tomato_crop() {
+		super(4, false);
 		this.setUnlocalizedName("tomatoCrop");
 		this.setCreativeTab(CreativeTabs.tabAllSearch);
+		
 	}
 	
 }
--- src/main/java/tk/mechtecs/organicmod/organicmod.java
+++ src/main/java/tk/mechtecs/organicmod/organicmod.java
@@ -24,7 +24,7 @@ import tk.mechtecs.organicmod.items.*;
 public class organicmod
 {
     public static final String MODID = "organicmod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.0.2";
     public static final String MODNAME = "The ORGANICS Mod";
     
     /*
